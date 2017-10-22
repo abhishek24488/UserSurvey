337f3e988cacab3bd5d0b5a3aed09a735ab0fed7
abhishek24488 <abhishek24488@gmail.com>
2017-10-23 01:04:47 +0530

    Update OAuth protocol

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+// User env file
+
+//const env= require('./dev');
+
+if(process.env.NODE_ENV === 'production'){
+    module.exports= require('./prod');
+}else{
+    module.exports= require('./dev');
+}
